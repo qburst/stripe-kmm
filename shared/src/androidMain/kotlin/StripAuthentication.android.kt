ac1a9efd27cf0c721030c066eef79aff7eb3fbd0
Abhishek Binsa <abhishekb@qburst.com>
2024-08-13 15:19:29 +0530

    Added flowController integration to android app

--- shared/src/androidMain/kotlin/StripAuthentication.android.kt
+++ shared/src/androidMain/kotlin/StripAuthentication.android.kt
@@ -1,61 +1,69 @@
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import com.stripe.android.PaymentConfiguration
+import com.stripe.android.paymentsheet.PaymentOptionCallback
 import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.PaymentSheetResultCallback
+import com.stripe.android.paymentsheet.PaymentSheet.FlowController
 import com.stripe.android.paymentsheet.PaymentSheetResult
 
-
 actual class StripAuthentication actual constructor() {
-    lateinit var paymentSheet: PaymentSheet
-    lateinit var configuration: PaymentSheet.CustomerConfiguration
+    private lateinit var flowController: FlowController
 
     actual suspend fun getCustomers(
         context: Any,
         activity: Any,
         clientSecret: String,
         publishableKey: String
-    ){
-        PaymentConfiguration.init(context = context as Context, publishableKey = publishableKey)
-
-        paymentSheet = PaymentSheet(activity as ComponentActivity) { paymentSheet ->
-            val result = onPaymentSheetResult(paymentSheet)
-            Toast.makeText(context, result, Toast.LENGTH_LONG).show()
-        }
-
-        presentPaymentSheet(
-            paymentSheet = paymentSheet,
-            clientSecret
-        )
-    }
-
-    private fun presentPaymentSheet(
-        paymentSheet: PaymentSheet,
-        paymentIntentClientSecret: String
     ) {
-        paymentSheet.presentWithPaymentIntent(
-            paymentIntentClientSecret,
-            PaymentSheet.Configuration(
-                merchantDisplayName = "Stripe Libray",
-                allowsDelayedPaymentMethods = true
-            )
+
+        fun showToast(message: String) {
+            Toast.makeText(context as Context, message, Toast.LENGTH_LONG).show()
+        }
+
+        val paymentOptionCallback = PaymentOptionCallback { paymentOption ->
+            showToast("Selected Payment Option: ${paymentOption?.toString() ?: "None"}")
+            flowController.confirm()
+        }
+
+        val paymentResultCallback = PaymentSheetResultCallback { paymentSheetResult ->
+            when (paymentSheetResult) {
+                is PaymentSheetResult.Canceled -> {
+                    showToast("Payment Cancelled")
+                }
+                is PaymentSheetResult.Failed -> {
+                    showToast("Payment Failed")
+                }
+                is PaymentSheetResult.Completed -> {
+                    showToast("Payment Completed")
+                }
+            }
+        }
+
+        val configComplete = FlowController.ConfigCallback {
+            isReady, error ->
+            showToast("isReady: $isReady, error: ${error?.message}")
+            handlePaymentSheet(isReady, error, flowController)
+        }
+
+        flowController = FlowController.create(
+            activity as ComponentActivity,
+            paymentOptionCallback,
+            paymentResultCallback
+        )
+
+        flowController.configureWithPaymentIntent(
+            clientSecret,
+            PaymentSheet.Configuration(merchantDisplayName = "Stripe Library"),
+            configComplete
         )
     }
 
-    private fun onPaymentSheetResult(paymentSheetResult: PaymentSheetResult): String {
-        when (paymentSheetResult) {
-            is PaymentSheetResult.Canceled -> {
-                return "cancelled"
-            }
-
-            is PaymentSheetResult.Failed -> {
-                return "Error: ${paymentSheetResult.error}"
-            }
-
-            is PaymentSheetResult.Completed -> {
-                return "Completed"
-            }
+    private fun handlePaymentSheet(isReady: Boolean, error: Throwable?, flowController: FlowController) {
+        if (isReady) {
+            flowController.presentPaymentOptions()
         }
+        else error?.printStackTrace()
     }
-
 }
