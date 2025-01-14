0173ef548c2ae560e9704aa84cd446ec1c62b569
abdulbasithqb <abdul.basith@qburst.com>
2025-01-14 17:34:59 +0530

    fix: config changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,5 @@
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
-    alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.kotlinJvm) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply  false
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
