0a6ca295b44574acde10740a2c106d290aff6734
abdulbasithqb <abdul.basith@qburst.com>
2025-01-14 17:05:06 +0530

    stripe kmm library publish poc

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,113 +1,101 @@
-
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
+import com.vanniktech.maven.publish.SonatypeHost
 plugins {
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25"
-    kotlin("plugin.allopen") version "2.0.20"
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
+group = "io.github.abdulbasithqb"
+version = "1.0.0"
+
+
 kotlin {
-
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        unitTestVariant {
-            dependencies {
-                testImplementation(libs.mockative)
-                testImplementation(kotlin("test-junit"))
-                testImplementation(libs.junit)
-                testImplementation(libs.kotlinx.coroutines.test)
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
             }
         }
     }
 
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-    jvm()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
 
     sourceSets {
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
-
-
+            //put your multiplatform dependencies here
         }
         iosMain.dependencies {
             implementation(project.dependencies.platform("io.insert-koin:koin-bom:4.0.0-RC2"))
             implementation("io.insert-koin:koin-core")
             implementation("io.insert-koin:koin-test")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
         }
         androidMain.dependencies {
             implementation (libs.stripe.android)
             implementation(libs.jetbrains.kotlinx.coroutines.core)
             implementation(libs.gson)
-
-
         }
-        iosTest.dependencies {
-            implementation(kotlin("test"))
-            implementation(libs.mockative)
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
         }
     }
-    task("testClasses")
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "shared"
-            isStatic = true
-        }
-    }
-}
-
-
-dependencies {
-    configurations
-        .filter { it.name.startsWith("ksp") && it.name.contains("Test") }
-        .forEach {
-            add(it.name, "io.mockative:mockative-processor:2.2.2")
-        }
 }
 
 android {
-    namespace = "com.qburst.stripe_kmm.shared"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+    namespace = "io.github.abdulbasithqb.stripekmm"
+    compileSdk = 34
     defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
+        minSdk = 30
     }
-    dependencies {
-        implementation(libs.androidx.activity.compose)
-    }
-}
-val taskIsRunningTest = gradle.startParameter.taskNames
-    .any { it == "check" || it.startsWith("test") || it.contains("Test") }
-
-if (taskIsRunningTest) {
-    allOpen {
-        annotation("Mockable")
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
-ksp {
-    arg("io.mockative:mockative:opt-in:io.github.OptInType", "kotlinx.cinterop.ExperimentalForeignApi")
-    arg("io.mockative:mockative:opt-in:io.github.*", "kotlin.ExperimentalStdlibApi")
-    arg("io.mockative:mockative:opt-in", "kotlin.ExperimentalUnsignedTypes")
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(group.toString(), "fibonacci", version.toString())
+
+    pom {
+        name = "Stripe KMM library"
+        description = "A stripe kmm library."
+        inceptionYear = "2024"
+        url = "https://github.com/qburst/stripe-kmm/"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "qburst"
+                name = "Qburst Open source community"
+                url = "https://github.com/qburst/"
+            }
+        }
+        scm {
+            url = "https://github.com/qburst/stripe-kmm/"
+            connection = "scm:git:git://github.com/qburst/stripe-kmm.git"
+            developerConnection = "scm:git:ssh://git@github.com/qburst/stripe-kmm.git"
+        }
+    }
 }
-
-
 
 
